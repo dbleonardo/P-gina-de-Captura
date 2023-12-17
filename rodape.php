7b4a98c854dd56e6cf2001d4ca83ddbcef524ec1
dbleonardo <davidbl.1281@gmail.com>
2023-12-17 17:21:10 -0300

    :up: atualização de padding do footer e feita o copyright.

--- rodape.php
+++ rodape.php
@@ -4,5 +4,8 @@
         <h3>O palestrante</h3>
         <p>O palestrante é conhecido por dar palestras que abalam a estrutura do Marketing tradicional.Prepare-se para sair do evento com uma nova visão do que é possívelser feeito para o seu negócio decolar.</p>
         <p>Formado em Marketing pela universidade XYZ e fundador da MarkUp, a empresa vencedora dos últimos 5 prêmios MarketingBr.</p>
-    </div>
+    </div><!-- description -->
+    <div class="copy">
+        <p style="color:white; margin-top:50px;">&copy; Todos os direitos reservados. 2023.</p>
+    </div><!-- copy -->
 </footer>
